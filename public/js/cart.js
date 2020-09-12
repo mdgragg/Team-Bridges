40447d7754148439525f68861f9d1b76d20a2d9e
Alex Fuss <alexfuss2@gmail.com>
2020-09-12 10:39:53 -0400

    Debugged the styling issue with spacing and worked to get the cart functioning in local storage

--- public/js/cart.js
+++ public/js/cart.js
@@ -1,118 +1,120 @@
-const shoppingCart = function() {
-  // Private methods and properties
-  let cart = [];
-  function Item(name, price, count) {
-    this.name = name;
-    this.price = price;
-    this.count = count;
-  }
-  function saveCart() {
-    localStorage.setItem("shoppingCart", JSON.stringify(cart));
-  }
-  function loadCart() {
-    cart = JSON.parse(localStorage.getItem("shoppingCart"));
-    if (cart === null) {
-      cart = [];
-    }
-  }
-  loadCart();
-  // Public methods and properties
-  const obj = {};
-  obj.addItemToCart = function(name, price, count) {
-    for (const i in cart) {
-      if (cart[i].name === name) {
-        cart[i].count += count;
-        saveCart();
-        return;
-      }
-    }
-    console.log("addItemToCart:", name, price, count);
-    const item = new Item(name, price, count);
-    cart.push(item);
-    saveCart();
-  };
-  obj.setCountForItem = function(name, count) {
-    for (const i in cart) {
-      if (cart[i].name === name) {
-        cart[i].count = count;
-        break;
-      }
-    }
-    saveCart();
-  };
-  obj.removeItemFromCart = function(name) {
-    // Removes one item
-    for (const i in cart) {
-      if (cart[i].name === name) {
-        // "3" === 3 false
-        cart[i].count--; // cart[i].count --
-        if (cart[i].count === 0) {
-          cart.splice(i, 1);
-        }
-        break;
-      }
-    }
-    saveCart();
-  };
-  obj.removeItemFromCartAll = function(name) {
-    // removes all item name
-    for (const i in cart) {
-      if (cart[i].name === name) {
-        cart.splice(i, 1);
-        break;
-      }
-    }
-    saveCart();
-  };
-  obj.clearCart = function() {
+// const shoppingCart = function() {
+// Private methods and properties
+let cart = [];
+function Item(name, price, count) {
+  this.name = name;
+  this.price = price;
+  this.count = count;
+}
+function saveCart() {
+  localStorage.setItem("shoppingCart", JSON.stringify(cart));
+}
+function loadCart() {
+  cart = JSON.parse(localStorage.getItem("shoppingCart"));
+  if (cart === null) {
     cart = [];
-    saveCart();
-  };
-  obj.countCart = function() {
-    // -> return total count
-    let totalCount = 0;
-    for (const i in cart) {
-      totalCount += cart[i].count;
+  }
+}
+loadCart();
+// Public methods and properties
+const shoppingCart = {};
+shoppingCart.addItemToCart = function(name, price, count) {
+  for (const i in cart) {
+    if (cart[i].name === name) {
+      cart[i].count += count;
+      saveCart();
+      alert("Item added to your cart!");
+      return;
     }
-    return totalCount;
-  };
-  obj.totalCart = function() {
-    // -> return total cost
-    let totalCost = 0;
-    for (const i in cart) {
-      totalCost += cart[i].price * cart[i].count;
-    }
-    return totalCost.toFixed(2);
-  };
-  obj.listCart = function() {
-    // -> array of Items
-    const cartCopy = [];
-    console.log("Listing cart");
-    console.log(cart);
-    for (const i in cart) {
-      console.log(i);
-      const item = cart[i];
-      const itemCopy = {};
-      for (const p in item) {
-        itemCopy[p] = item[p];
-      }
-      itemCopy.total = (item.price * item.count).toFixed(2);
-      cartCopy.push(itemCopy);
-    }
-    return cartCopy;
-  };
-  // ----------------------------
-  return obj;
+  }
+  console.log("addItemToCart:", name, price, count);
+  const item = new Item(name, price, count);
+  cart.push(item);
+  saveCart();
 };
-shoppingCart();
+shoppingCart.setCountForItem = function(name, count) {
+  for (const i in cart) {
+    if (cart[i].name === name) {
+      cart[i].count = count;
+      break;
+    }
+  }
+  saveCart();
+};
+shoppingCart.removeItemFromCart = function(name) {
+  // Removes one item
+  for (const i in cart) {
+    if (cart[i].name === name) {
+      // "3" === 3 false
+      cart[i].count--; // cart[i].count --
+      if (cart[i].count === 0) {
+        cart.splice(i, 1);
+      }
+      break;
+    }
+  }
+  saveCart();
+};
+shoppingCart.removeItemFromCartAll = function(name) {
+  // removes all item name
+  for (const i in cart) {
+    if (cart[i].name === name) {
+      cart.splice(i, 1);
+      break;
+    }
+  }
+  saveCart();
+};
+shoppingCart.clearCart = function() {
+  cart = [];
+  saveCart();
+};
+shoppingCart.countCart = function() {
+  // -> return total count
+  let totalCount = 0;
+  for (const i in cart) {
+    totalCount += cart[i].count;
+  }
+  return totalCount;
+};
+shoppingCart.totalCart = function() {
+  // -> return total cost
+  let totalCost = 0;
+  for (const i in cart) {
+    totalCost += cart[i].price * cart[i].count;
+  }
+  return totalCost.toFixed(2);
+};
+shoppingCart.listCart = function() {
+  // -> array of Items
+  const cartCopy = [];
+  console.log("Listing cart");
+  console.log(cart);
+  for (const i in cart) {
+    console.log(i);
+    const item = cart[i];
+    const itemCopy = {};
+    for (const p in item) {
+      itemCopy[p] = item[p];
+    }
+    itemCopy.total = (item.price * item.count).toFixed(2);
+    cartCopy.push(itemCopy);
+  }
+  return cartCopy;
+};
+// ----------------------------
+// return obj;
+// };
+// shoppingCart();
 
 // EVENT LISTENING JS
-$(".add-to-cart").click(function(event) {
+$(document).on("click", ".add-to-cart", event => {
   event.preventDefault();
-  const name = $(this).attr("data-name");
-  const price = Number($(this).attr("item-price"));
+  const name = $(event.target).attr("data-name");
+  const price = Number($(event.target).attr("data-price"));
   shoppingCart.addItemToCart(name, price, 1);
   displayCart();
+  console.log("test");
 });
 $("#clear-cart").click(() => {
   shoppingCart.clearCart().empty(), displayCart();
